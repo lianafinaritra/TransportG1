c0326cf8c6300843e9b114a6d0df897c8b62a6d8
Liana Finaritra <100028310+lianafinaritra@users.noreply.github.com>
2022-06-09 11:20:54 +0300

    delete files

--- compter_reservation_non_paye.sql
+++
@@ -1,7 +0,0 @@
-
--- medium
--- compte le nombre de clients qui n'ont pas encore payé l'integralité de leurs frais
-SELECT count(*) FROM client c
-INNER JOIN reserver on reserver.id_client = client.id_client
-INNER JOIN offre on offre.id_offre = reserver.id_offre
-WHERE tarif > montant_paye;
