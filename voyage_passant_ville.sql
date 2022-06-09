3256ec8d350fb380b6db83918843d220852ebd7d
Liana Finaritra <100028310+lianafinaritra@users.noreply.github.com>
2022-06-09 11:22:09 +0300

    delete files

--- voyage_passant_ville.sql
+++
@@ -1,7 +0,0 @@
-
--- easy
--- affiche les voyages passant par une ville définie
-SELECT jour_depart, id_voyage FROM voyage
-INNER JOIN passer_par ON passer_par.id_voyage = voyage.id_voyage
-INNER JOIN ville ON passer_par.id_ville = ville.id_ville
-WHERE nom_ville = 'Pacora';
