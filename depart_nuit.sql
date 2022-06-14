c02804908a7b5d0194ba07be7a7ac26195dd9695
lianafinaritra <hei.lianafinaritra@gmail.com>
2022-06-14 14:35:15 +0300

    fix: update files

--- depart_nuit.sql
+++
@@ -1,6 +0,0 @@
-
--- basic
--- affiche le jour, l'heure et la ville de depart des voyages de nuits
-SELECT jour_depart, heure_depart, nom_ville FROM voyage
-INNER JOIN ville ON voyage.id_ville_depart = ville.id_ville
-WHERE heure_depart > '18:00';
--- voyage_durant_semaine.sql
+++
@@ -1,5 +0,0 @@
-
--- medium/hard
--- affiche tous les voyages durant une semaine
-SELECT * from voyage
-WHERE jour_depart BETWEEN date(now()) AND (date(now())+1);
